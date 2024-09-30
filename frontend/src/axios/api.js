b421a82cda66a9372c509f1211f34e41ee80a78d
Fasna123 <fasnanishar@gmail.com>
2024-10-01 00:34:43 +0530

    minor updates

--- frontend/src/axios/api.js
+++ frontend/src/axios/api.js
@@ -1,31 +1,28 @@
 import axios from "axios";
 
 const token = localStorage.getItem("PerfumzToken");
-console.log("perfumztoken", token);
+//console.log("perfumztoken", token);
 const baseURI = import.meta.env.VITE_PUBLIC_BASE_API;
 
 const api = axios.create({
   baseURL: `${baseURI}/api/`,
   headers: {
     "Content-Type": "application/json",
-    Authorization: token || "",
   },
   withCredentials: true,
 });
-//api.defaults.withCredentials = true;
-console.log(api);
 
-// api.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem("PerfumzToken");
-//     if (token) {
-//       config.headers.Authorization = token;
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("PerfumzToken");
+    if (token) {
+      config.headers.Authorization = token;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
