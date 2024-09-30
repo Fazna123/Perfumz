ed693595ca5e894380db4d6824a0c4002c4e514c
Fasna123 <fasnanishar@gmail.com>
2024-09-30 20:56:13 +0530

    updates

--- backend/index.js
+++ backend/index.js
@@ -27,50 +27,50 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(
-//   cors({
-//     origin: "https://perfumz.vercel.app",
-//   })
-// );
+app.use(
+  cors({
+    origin: "https://perfumz.vercel.app/",
+  })
+);
 
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "https://perfumz.vercel.app");
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   res.setHeader("Access-Control-Allow-Credentials", "true");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://perfumz.vercel.app/");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "https://perfumz.vercel.app");
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://perfumz.vercel.app/");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
-// // Handle preflight requests
-// app.options("*", (req, res) => {
-//   res.header("Access-Control-Allow-Origin", "https://perfumz.vercel.app");
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   res.sendStatus(200);
-// });
+// Handle preflight requests
+app.options("*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "https://perfumz.vercel.app/");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.sendStatus(200);
+});
 
-const corsOptions = {
-  origin: "https://perfumz.vercel.app",
-  methods: "GET,HEAD,POST,PUT,DELETE,OPTIONS",
-  //allowedHeaders: "Content-Type, Authorization",
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: "https://perfumz.vercel.app",
+//   methods: "GET,HEAD,POST,PUT,DELETE,OPTIONS",
+//allowedHeaders: "Content-Type, Authorization",
+//   credentials: true,
+// };
 
 // Enable CORS middleware with options
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
 
 app.use("/api/products", productRoute);
 app.use("/api/inventory", inventoryRoute);
